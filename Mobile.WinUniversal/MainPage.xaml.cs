fef3d568341e1fe5d813c5a2ba2743d9d6c88e9a
adamped <adam.pedley@gmail.com>
2015-08-18 17:07:46 +1000

    Windows Universal Support

---
+++ Mobile.WinUniversal/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace Mobile.WinUniversal
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : global::Xamarin.Forms.Platform.WinRT.WindowsPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            
+            LoadApplication(new Mobile.App());
+        }
+    }
+}
